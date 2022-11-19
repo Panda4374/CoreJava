909e1b90fa01bb14b540b661fbdd7da2af05a675
TheAceEB <eesadipb@gmail.com>
2022-11-19 17:12:54 +0530

    Added examples of Date andTime

---
+++ src/DateAndTime.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+public class DateAndTime {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now(); //current time
+        LocalDate someDate = LocalDate.of(2022, 11, 19); //a specific date
+        System.out.println(localDate);
+
+        LocalTime localTime = LocalTime.now(); //current time
+        LocalTime noon = LocalTime.NOON; //this is a constant
+        System.out.println(localTime);
+
+        LocalDateTime localDateTime = LocalDateTime.now(); //current date time
+        System.out.println(localDateTime);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("E, MMM dd yyyy HH:mm:ss"); //formats the date and time
+        String formattedDate = localDateTime.format(dateTimeFormatter);
+        System.out.println(formattedDate);
+    }
+}
